ce7b64f33764c4c8fdfc166cc52f7d9ba8a5c8f0
MikeYC-Wang <asd384639@gmail.com>
2026-02-23 22:55:45 +0800

    update

--- backend/main.py
+++ backend/main.py
@@ -177,9 +177,15 @@ app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://mikeyc-wang.github.io", 
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
