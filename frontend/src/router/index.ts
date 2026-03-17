efd67fe7f9f798fe5319319d90fd8fc033c881be
MikeYC-Wang <asd384639@gmail.com>
2026-03-17 11:37:46 +0800

    update

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,4 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LabView from '@/views/LabView.vue'
 import BlogView from '@/views/BlogView.vue'
@@ -12,8 +11,7 @@ import ProjectView from '@/views/ProjectView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/lab', name: 'lab', component: LabView },
